4eab9d41ae08c41e93a464e2d0b6a35f100f602f
Ranjithanbu <154120836+Ranjithanbu@users.noreply.github.com>
2024-02-08 13:55:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function getweather(){
+
+let city=document.getElementById("ip1").value;
+
+fetch(`https://api.weatherapi.com/v1/current.json?key=b626473b4ea344dbad3210850240702&q=${city}&aqi=yes`).then(val=>val.json()).then(ans=>{
+let data=`
+<div class="col">
+<h4>Name : ${ans.location.name}</h4>
+<h4>Country : ${ans.location.country}</h4>
+<h4>Region : ${ans.location.region}</h4>
+<h4>Local-Time : ${ans.location.localtime}</h4>
+<h4>Temprature : ${ans.current.temp_c}<sup>0</sup> C</h4>
+<h4>Climate : ${ans.current.cloud}</h4>
+<h4>Humidity : ${ans.current.humidity} %</h4>
+<h4>Wind-Speed : ${ans.current.wind_kph} kph</h4>
+<h4>Air-Quality : ${ans.current.air_quality.co}</h4>
+
+</div>`
+
+console.log(data)
+document.getElementById("div2").innerHTML=data;
+
+})
+
+}
+
+//https://goweather.herokuapp.com/weather/{city}
+
